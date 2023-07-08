113c2e7112bc8885c3d481ea70da7707cb99fb27
Sri <srimathiselvaraj@gmaicom>
2023-07-08 22:00:47 +0530

    Auto Push

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,10 +1,26 @@
+def lintchecks(){
+    sh ***
               sh "echo Installing Jslint"
               sh "npm i jslint"
               sh "node_modules/jslint/bin/jslint.js server.js || true"
-def info(message) {
-    echo "Hai, I am function who name is info"
-    eho"I am printing the value of meassage is ${message}"
+    ***
 }
-
-//calling the function info
-info("Hello")
+def call(){
+    pipeline {
+    agent { label 'WS' }
+    stages {           ///
+        stage('Lint Checks') {
+            steps {
+                   script {
+                       nodejs.lintchecks()
+                  }
+            }         /////
+        }
+        stage ('Code Compile') {
+            steps{
+                sh "npm install"
+            }
+        }
+    }
+}
+}
