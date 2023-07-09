8e35c8e14215635409b155fa43b70b121640db20
Sri <srimathiselvaraj@gmaicom>
2023-07-09 18:08:04 +0530

    Auto Push

--- vars/java.groovy
+++ vars/java.groovy
@@ -1,26 +1,31 @@
 def lintchecks(){
-    sh ***
-              echo performing litchecks for ${COMPONENT}
-              mvn checkstyle:check || true
-              echo lint checks completed for ${COMPONENT}
-    ***
+    sh  "echo performing litchecks for ${COMPONENT}"
+    sh  "mvn checkstyle:check || true"
+    sh  "echo lint checks completed for ${COMPONENT} "
 }
-def call(){
+
+def call(COMPONENT){
     pipeline {
-    agent { label 'WS' }
-    stages {           ///
-        stage('Lint Checks') {
-            steps {
-                   script {
-                       lintchecks()
-                  }
-            }         /////
+        agent { label 'WS' }
+        environment {
+            SONARCRED = credentials('SONARCRED')
+            SONARURL = "172.31.84.15"
         }
-        stage ('Code Compile') {
-            steps{
-                sh "mvn clean compile"
-            }
+        stages {
+            stage('Lint Checks') {
+                 steps {
+                     script {
+                          lintChecks()
+                  }
+                 }
+            }        
         }
     }
 }
+        stage ('Sonar Checks') {
+                  steps{
+                     script {
+                          common.sonarChecks()
+                }
+            }
 }
--- vars/python.groovy
+++ vars/python.groovy
@@ -1,10 +1,8 @@
 def lintchecks(){
-    sh ***
-              echo performing Pylint for ${COMPONENT}
-              pip3 install Pylint
-              pylint *.py
-              echo lint checks completed for ${COMPONENT}
-    ***
+    sh "echo performing Pylint for ${COMPONENT}"
+    sh "pip3 install Pylint "
+    sh "pylint *.py "
+    sh "echo lint checks completed for ${COMPONENT} "
 }
 def call(){
     pipeline {
@@ -15,13 +13,21 @@ def call(){
                    script {
                        lintchecks()
                   }
-            }         /////
-        }
-        stage ('Code Compile') {
-            steps{
-                sh "mvn clean compile"
-            }
+                } 
+            }        /////
         }
     }
-}
+        stage ('Sonar Checks') {
+                  steps{
+                     script {
+                          common.sonarChecks()
+                        }
+                  }
+        }
+
+        stage ('Testing') {
+                 steps{
+                     sh "echo Testing in-progress"
+                }
+        }
 }
